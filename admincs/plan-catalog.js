1b3a267f9ff84dccb7d6f173a75cf3cf94250573
sunsun042505 <108930636+sunsun042505@users.noreply.github.com>
2026-05-03 16:31:49 +0900

    Add files via upload

---
+++ admincs/plan-catalog.js
@@ -0,0 +1,402 @@
+window.NEVERLAB_PLAN_CATALOG = [
+  {
+    "name": "SKT 5GX 프리미엄",
+    "price": 109000
+  },
+  {
+    "name": "SKT 5GX 프라임플러스",
+    "price": 99000
+  },
+  {
+    "name": "SKT 5GX 프라임",
+    "price": 89000
+  },
+  {
+    "name": "SKT 5GX 레귤러플러스",
+    "price": 79000
+  },
+  {
+    "name": "SKT 5GX 레귤러",
+    "price": 69000
+  },
+  {
+    "name": "SKT 5G 베이직플러스",
+    "price": 59000
+  },
+  {
+    "name": "SKT 5G 베이직",
+    "price": 49000
+  },
+  {
+    "name": "SKT 다이렉트5G 76",
+    "price": 76000
+  },
+  {
+    "name": "SKT 다이렉트5G 69",
+    "price": 69000
+  },
+  {
+    "name": "SKT 다이렉트5G 62",
+    "price": 62000
+  },
+  {
+    "name": "SKT 다이렉트5G 55",
+    "price": 55000
+  },
+  {
+    "name": "SKT 다이렉트5G 48",
+    "price": 48000
+  },
+  {
+    "name": "SKT 0청년 109",
+    "price": 109000
+  },
+  {
+    "name": "SKT 0청년 99",
+    "price": 99000
+  },
+  {
+    "name": "SKT 0청년 79",
+    "price": 79000
+  },
+  {
+    "name": "SKT 0청년 69",
+    "price": 69000
+  },
+  {
+    "name": "SKT 0청년 59",
+    "price": 59000
+  },
+  {
+    "name": "SKT 0틴 5G",
+    "price": 45000
+  },
+  {
+    "name": "SKT 5G ZEM 퍼펙트",
+    "price": 36000
+  },
+  {
+    "name": "SKT 주말엔 팅 3GB",
+    "price": 41000
+  },
+  {
+    "name": "SKT LTE 데이터ON 프리미엄",
+    "price": 89000
+  },
+  {
+    "name": "SKT LTE 데이터ON 스탠다드",
+    "price": 69000
+  },
+  {
+    "name": "SKT LTE 세이브",
+    "price": 33000
+  },
+  {
+    "name": "SKT 시니어 안심 4GB",
+    "price": 22000
+  },
+  {
+    "name": "SKT 복지 5G 라이트",
+    "price": 33000
+  },
+  {
+    "name": "KT 5G 스페셜",
+    "price": 100000
+  },
+  {
+    "name": "KT 5G 베이직",
+    "price": 80000
+  },
+  {
+    "name": "KT 5G 심플 110GB",
+    "price": 69000
+  },
+  {
+    "name": "KT 5G 심플 70GB",
+    "price": 65000
+  },
+  {
+    "name": "KT 5G 심플 50GB",
+    "price": 63000
+  },
+  {
+    "name": "KT 5G 심플 30GB",
+    "price": 61000
+  },
+  {
+    "name": "KT 5G 슬림 21GB",
+    "price": 55000
+  },
+  {
+    "name": "KT 5G 슬림 14GB",
+    "price": 47000
+  },
+  {
+    "name": "KT 5G 슬림 10GB",
+    "price": 45000
+  },
+  {
+    "name": "KT 5G 슬림 4GB",
+    "price": 37000
+  },
+  {
+    "name": "KT 5G 슬림 이월 21GB",
+    "price": 56000
+  },
+  {
+    "name": "KT 5G 슬림 이월 14GB",
+    "price": 48000
+  },
+  {
+    "name": "KT LTE 데이터ON 프리미엄",
+    "price": 89000
+  },
+  {
+    "name": "KT LTE 데이터ON 비디오",
+    "price": 69000
+  },
+  {
+    "name": "KT LTE 데이터ON 톡",
+    "price": 49000
+  },
+  {
+    "name": "KT LTE 베이직",
+    "price": 33000
+  },
+  {
+    "name": "KT LTE Y 무제한",
+    "price": 69000
+  },
+  {
+    "name": "KT LTE Y 10GB",
+    "price": 49000
+  },
+  {
+    "name": "KT LTE Y 2.5GB",
+    "price": 33000
+  },
+  {
+    "name": "KT 시니어 베이직",
+    "price": 22000
+  },
+  {
+    "name": "KT 시니어 데이터안심",
+    "price": 33000
+  },
+  {
+    "name": "KT 복지 데이터안심",
+    "price": 33000
+  },
+  {
+    "name": "KT 키즈 알 115",
+    "price": 28000
+  },
+  {
+    "name": "KT 군인 Y군인 77",
+    "price": 77000
+  },
+  {
+    "name": "KT 군인 Y군인 55",
+    "price": 55000
+  },
+  {
+    "name": "U+ 5G 시그니처",
+    "price": 130000
+  },
+  {
+    "name": "U+ 5G 프리미어 슈퍼",
+    "price": 115000
+  },
+  {
+    "name": "U+ 5G 프리미어 플러스",
+    "price": 105000
+  },
+  {
+    "name": "U+ 5G 프리미어 레귤러",
+    "price": 95000
+  },
+  {
+    "name": "U+ 5G 프리미어 에센셜",
+    "price": 85000
+  },
+  {
+    "name": "U+ 5G 스탠다드",
+    "price": 75000
+  },
+  {
+    "name": "U+ 5G 데이터 슈퍼",
+    "price": 68000
+  },
+  {
+    "name": "U+ 5G 데이터 플러스",
+    "price": 61000
+  },
+  {
+    "name": "U+ 5G 라이트+",
+    "price": 55000
+  },
+  {
+    "name": "U+ 5G 라이트",
+    "price": 47000
+  },
+  {
+    "name": "U+ 다이렉트 5G 69",
+    "price": 69000
+  },
+  {
+    "name": "U+ 다이렉트 5G 59",
+    "price": 59000
+  },
+  {
+    "name": "U+ 다이렉트 5G 51",
+    "price": 51000
+  },
+  {
+    "name": "U+ 다이렉트 5G 44",
+    "price": 44000
+  },
+  {
+    "name": "U+ LTE 프리미어 플러스",
+    "price": 89000
+  },
+  {
+    "name": "U+ LTE 추가요금 걱정없는 데이터 69",
+    "price": 69000
+  },
+  {
+    "name": "U+ LTE 추가요금 걱정없는 데이터 59",
+    "price": 59000
+  },
+  {
+    "name": "U+ LTE 데이터 33",
+    "price": 33000
+  },
+  {
+    "name": "U+ 청소년 5G 라이트",
+    "price": 45000
+  },
+  {
+    "name": "U+ 키즈 5G",
+    "price": 29000
+  },
+  {
+    "name": "U+ 시니어 5G 안심",
+    "price": 45000
+  },
+  {
+    "name": "U+ 시니어 LTE 안심",
+    "price": 33000
+  },
+  {
+    "name": "U+ 복지 LTE 라이트",
+    "price": 22000
+  },
+  {
+    "name": "U+ 현역병 5G 55",
+    "price": 55000
+  },
+  {
+    "name": "U+ 태블릿 5G 4GB",
+    "price": 22000
+  },
+  {
+    "name": "알뜰 LTE 1GB 100분",
+    "price": 3900
+  },
+  {
+    "name": "알뜰 LTE 3GB 200분",
+    "price": 5900
+  },
+  {
+    "name": "알뜰 LTE 6GB 350분",
+    "price": 9900
+  },
+  {
+    "name": "알뜰 LTE 7GB+1Mbps",
+    "price": 19800
+  },
+  {
+    "name": "알뜰 LTE 10GB+1Mbps",
+    "price": 22000
+  },
+  {
+    "name": "알뜰 LTE 11GB+2GB+3Mbps",
+    "price": 33000
+  },
+  {
+    "name": "알뜰 LTE 15GB 350분",
+    "price": 22000
+  },
+  {
+    "name": "알뜰 LTE 15GB+3Mbps",
+    "price": 27500
+  },
+  {
+    "name": "알뜰 LTE 100GB+5Mbps",
+    "price": 39900
+  },
+  {
+    "name": "알뜰 5G 9GB",
+    "price": 19000
+  },
+  {
+    "name": "알뜰 5G 30GB",
+    "price": 27500
+  },
+  {
+    "name": "알뜰 5G 50GB",
+    "price": 33000
+  },
+  {
+    "name": "알뜰 5G 100GB",
+    "price": 44000
+  },
+  {
+    "name": "알뜰 5G 무제한",
+    "price": 55000
+  },
+  {
+    "name": "네버랩 5G 스타터",
+    "price": 29000
+  },
+  {
+    "name": "네버랩 5G 라이트",
+    "price": 39000
+  },
+  {
+    "name": "네버랩 5G 베이직",
+    "price": 49000
+  },
+  {
+    "name": "네버랩 5G 스탠다드",
+    "price": 59000
+  },
+  {
+    "name": "네버랩 5G 프리미엄",
+    "price": 79000
+  },
+  {
+    "name": "네버랩 LTE 절약 1GB",
+    "price": 9900
+  },
+  {
+    "name": "네버랩 LTE 실속 5GB",
+    "price": 19900
+  },
+  {
+    "name": "네버랩 LTE 안심 10GB",
+    "price": 29900
+  },
+  {
+    "name": "네버랩 시니어 안심콜",
+    "price": 14900
+  },
+  {
+    "name": "네버랩 복지할인 라이트",
+    "price": 19900
+  },
+  {
+    "name": "네버랩 현역병 안심 55",
+    "price": 55000
+  }
+];
